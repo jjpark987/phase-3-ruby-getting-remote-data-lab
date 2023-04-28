0112386c0bfa9c925f7346e799f8e2b41c3bfebb
Jason Park <jjpark987@gmail.com>
2023-04-28 11:55:35 -0700

    commit tree 5cf74a8f69dfcfde42bf45b45704bbb64394a597

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+
+    attr_reader :url
+
+    def initialize(url_string)
+        @url = url_string
+    end
+
+    # Sends a GET request to the URL and returns the body of the response
+    def get_response_body
+        Net::HTTP.get_response(URI.parse(@url)).body
+    end
+
+    # Returns a Ruby Array or Hash made up of data converted from the response
+    def parse_json
+        JSON.parse(self.get_response_body)
+    end
+
+end
